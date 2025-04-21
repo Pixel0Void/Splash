3113f70b107fa75ea61c7e01c0e1dcd3c2d8ba35
PixelVoid <Pixel0Void@gmail.com>
2025-04-21 17:46:55 +0430

    fix: camera size for larger levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,7 +8,7 @@ using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
     public GameObject PlayerPrefab;
-    public List<Transform> Levels = new List<Transform>();
+    public List<Level> Levels = new List<Level>();
     public UnityEvent OnLevelFinished;
 
     private int m_CurrentLevel = 0;
@@ -31,13 +32,18 @@ public class GameManager : MonoBehaviour
     {
         m_CurrentLevel = index;
 
-        Vector3 entryPointPos = Levels[m_CurrentLevel].GetComponentsInChildren<Transform>().Where(t => t.name.Contains("EntryPoint")).FirstOrDefault().position;
+        Vector3 entryPointPos = Levels[m_CurrentLevel].LevelObject.GetComponentsInChildren<Transform>().Where(t => t.name.Contains("EntryPoint")).FirstOrDefault().position;
         m_PlayerClone = Instantiate(PlayerPrefab, entryPointPos, Quaternion.identity);
         m_PlayerController = m_PlayerClone.GetComponent<PlayerController>();
-        m_LevelClone = Instantiate(Levels[m_CurrentLevel].gameObject, Vector3.zero, Quaternion.identity);
+        m_LevelClone = Instantiate(Levels[m_CurrentLevel].LevelObject, Vector3.zero, Quaternion.identity);
 
         m_LevelTiles = m_LevelClone.GetComponentsInChildren<Transform>().Where(t => t.name.Contains("Tile")).ToList();
         TilesCount = m_LevelTiles.Count;
+
+        if (Levels[m_CurrentLevel].IsBig)
+            CameraController.LargeLevelCameraSetup();
+        else
+            CameraController.SmallLevelCameraSetup();
     }
 
     public void MoveToNextLevel()
@@ -60,4 +66,11 @@ public class GameManager : MonoBehaviour
     {
         m_CurrentLevel = PlayerPrefs.GetInt("CurrentLevel");
     }
+}
+
+[Serializable]
+public struct Level
+{
+    public GameObject LevelObject;
+    public bool IsBig;
 }
