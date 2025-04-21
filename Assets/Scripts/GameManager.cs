1fee4fb680f2c739c13bf5a5142a75d91f0bf778
PixelVoid <Pixel0Void@gmail.com>
2025-04-21 16:14:55 +0430

    feat: create player prefab, instantiate level in scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    public List<Transform> Levels = new List<Transform>();
+    private int m_CurrentLevel = 0;
+
+    private GameObject m_PlayerClone;
+    private GameObject m_LevelClone;
+
+    private void Start()
+    {
+        InitialLevel(15);
+    }
+
+    private void InitialLevel(int index)
+    {
+        m_CurrentLevel = index;
+
+        Vector3 entryPointPos = Levels[m_CurrentLevel].GetComponentsInChildren<Transform>().Where(t => t.name.Contains("EntryPoint")).FirstOrDefault().position;
+        m_PlayerClone = Instantiate(PlayerPrefab, entryPointPos, Quaternion.identity);
+        m_LevelClone = Instantiate(Levels[m_CurrentLevel].gameObject, Vector3.zero, Quaternion.identity);
+    }
+}
