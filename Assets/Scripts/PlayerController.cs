4ab34f4b5af23e0b9fda0bf4fe49b591346af5f3
PixelVoid <Pixel0Void@gmail.com>
2025-04-21 15:44:53 +0430

    fix: move player to an inaccurate position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,29 +7,31 @@ public class PlayerController : MonoBehaviour
     private Vector3 m_Direction;
     private Vector3 m_TargetPosition;
     private bool m_RecivingInput = true;
+    private bool m_CanMove = false;
     private float m_Speed = 20f;
 
     private void Update()
     {
-        if(m_RecivingInput == false)
+        if(m_RecivingInput == false && m_CanMove)
         {
             Move();
         }
         SetDirection();
     }
 
-    private Vector3 GetTargetPosition()
+    private bool GetTargetPosition(out Vector3 targetPos)
     {
-        Vector3 target = new Vector3();
         RaycastHit hit;
         if(Physics.Raycast(transform.position, m_Direction, out hit,100f))
         {
             if(hit.transform.CompareTag("Wall"))
             {
-                target = hit.transform.position;
+                targetPos = hit.transform.position;
+                return true;
             }
         }
-        return target;
+        targetPos = new Vector3();
+        return false;
     }
 
     private void SetDirection()
@@ -39,29 +41,29 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.A))
             {
                 m_Direction = Vector3.left;
-                m_TargetPosition = GetTargetPosition();
-                m_RecivingInput = false;
+                m_CanMove = GetTargetPosition(out m_TargetPosition);
+                m_RecivingInput = !m_CanMove;
             }
 
             if (Input.GetKeyDown(KeyCode.W))
             {
                 m_Direction = Vector3.forward;
-                m_TargetPosition = GetTargetPosition();
-                m_RecivingInput = false;
+                m_CanMove = GetTargetPosition(out m_TargetPosition);
+                m_RecivingInput = !m_CanMove;
             }
 
             if (Input.GetKeyDown(KeyCode.D))
             {
                 m_Direction = Vector3.right;
-                m_TargetPosition = GetTargetPosition();
-                m_RecivingInput = false;
+                m_CanMove = GetTargetPosition(out m_TargetPosition);
+                m_RecivingInput = !m_CanMove;
             }
 
             if (Input.GetKeyDown(KeyCode.S))
             {
                 m_Direction = Vector3.back;
-                m_TargetPosition = GetTargetPosition();
-                m_RecivingInput = false;
+                m_CanMove = GetTargetPosition(out m_TargetPosition);
+                m_RecivingInput = !m_CanMove;
             }
         }
     }
