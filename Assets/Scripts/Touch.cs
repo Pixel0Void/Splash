36f3f359b8203f55a8d7a95f1fab41e1f5c80f1b
PixelVoid <Pixel0Void@gmail.com>
2025-04-22 12:06:28 +0430

    feat: add touch panel, set direction to move the player

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Touch : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Vector2 m_StartPosition;
+    private Vector2 m_EndPosition;
+
+    [SerializeField] private Vector3 m_Direction;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        m_StartPosition = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        m_EndPosition = eventData.position;
+        CalculateDirection();
+    }
+
+    private void CalculateDirection()
+    {
+        m_Direction = (m_EndPosition - m_StartPosition).normalized;
+
+        if (Mathf.Abs(m_Direction.x) > Mathf.Abs(m_Direction.y))
+        {
+            if (m_Direction.x > 0)
+            {
+                m_Direction = Vector3.right;
+            }
+            else
+            {
+                m_Direction = Vector3.left;
+            }
+        }
+        else
+        {
+            if (m_Direction.y > 0)
+            {
+                m_Direction = Vector3.forward;
+            }
+            else
+            {
+                m_Direction = Vector3.back;
+            }
+        }
+    }
+}
